ed2aa32b90ac41fc3024640eae542bd927d04ca8
unknown <parthiban.kesavan@royalcyber.com>
2020-07-28 15:39:05 +0400

    first commit

---
+++ src/components/Cart/CartTotals.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PayPalButton from "./PayPalButton";
+
+export default function CartTotals({ value }) {
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right"
+            onClick={() => clearCart()}
+          >
+            <Link to="/">
+              <button
+                className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                type="button"
+              >
+                clear cart
+              </button>
+            </Link>
+            <h5 className="text-blue">
+              <span>subtotal : </span>
+              <strong>${cartSubTotal}</strong>
+            </h5>
+            <h5 className="text-blue">
+              <span>tax : </span>
+              <strong>${cartTax}</strong>
+            </h5>
+            <h5 className="text-blue">
+              <span>total : </span>
+              <strong>${cartTotal}</strong>
+            </h5>
+            <PayPalButton />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
